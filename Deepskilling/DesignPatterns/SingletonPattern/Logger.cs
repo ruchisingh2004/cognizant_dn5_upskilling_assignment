7c39688de0bf569be05071bb4256d4597d134939
Ruchi Singh <riya.ruchi09@gmail.com>
2026-06-20 23:55:13 +0530

    Added deepskilling hands-on projects

---
+++ Deepskilling/DesignPatterns/SingletonPattern/Logger.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace SingletonPattern
+{
+    public sealed class Logger
+    {
+        private static Logger instance;
+
+        private Logger()
+        {
+        }
+
+        public static Logger GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Logger();
+            }
+
+            return instance;
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Deepskilling/DesignPatterns/SingletonPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger1 = Logger.GetInstance();
+            Logger logger2 = Logger.GetInstance();
+
+            Console.WriteLine(Object.ReferenceEquals(logger1, logger2));
+
+            logger1.Log("Singleton Pattern Working");
+        }
+    }
+}
