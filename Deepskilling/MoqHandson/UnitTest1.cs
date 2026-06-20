ec80d6f5172bf4b86ff3f9cef06a0c81de8c762a
Ruchi Singh <riya.ruchi09@gmail.com>
2026-06-21 00:26:19 +0530

    Completed Moq hands-on

---
+++ Deepskilling/MoqHandson/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using Moq;
+using NUnit.Framework;
+
+public interface IMailSender
+{
+    bool SendMail(string toAddress, string message);
+}
+
+public class CustomerComm
+{
+    private readonly IMailSender _mailSender;
+
+    public CustomerComm(IMailSender mailSender)
+    {
+        _mailSender = mailSender;
+    }
+
+    public bool SendMailToCustomer()
+    {
+        return _mailSender.SendMail(
+            "cust123@abc.com",
+            "Some Message");
+    }
+}
+
+[TestFixture]
+public class MailSenderTests
+{
+    [Test]
+    public void TestSendMail()
+    {
+        var mockMailSender = new Mock<IMailSender>();
+
+        mockMailSender
+            .Setup(x => x.SendMail(
+                It.IsAny<string>(),
+                It.IsAny<string>()))
+            .Returns(true);
+
+        CustomerComm customer =
+            new CustomerComm(mockMailSender.Object);
+
+        Assert.That(
+            customer.SendMailToCustomer(),
+            Is.True);
+    }
+}
