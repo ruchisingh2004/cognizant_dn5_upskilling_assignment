0019d88d31bac2fc6e283f9e340997eb83aa24f8
Ruchi Singh <riya.ruchi09@gmail.com>
2026-06-24 17:01:24 +0530

    Completed EF Core Labs 1-4

--- Deepskilling/EFcore8/RetailInventory/Program.cs
+++ Deepskilling/EFcore8/RetailInventory/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using RetailInventory.Data;
+using RetailInventory.Models;
+
+using var context = new AppDbContext();
+
+var electronics = new Category
+{
+    Name = "Electronics"
+};
+
+var groceries = new Category
+{
+    Name = "Groceries"
+};
+
+context.Categories.AddRange(electronics, groceries);
+
+var product1 = new Product
+{
+    Name = "Laptop",
+    Price = 75000,
+    Category = electronics
+};
+
+var product2 = new Product
+{
+    Name = "Rice Bag",
+    Price = 1200,
+    Category = groceries
+};
+
+context.Products.AddRange(product1, product2);
+
+context.SaveChanges();
+
+Console.WriteLine("Data Inserted Successfully");
